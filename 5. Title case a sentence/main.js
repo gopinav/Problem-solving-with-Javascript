01fef1bb45fac15b76b9e2694a06b25b22fe33b2
Vishwas <gopinav5@gmail.com>
2018-05-20 15:35:28 +0530

    Title case a sentence

---
+++ 5. Title case a sentence/main.js
@@ -0,0 +1,23 @@
+// 1. With a for loop
+
+function titleCase(str) {
+  str = str.toLowerCase().split(' ');
+  for (var i = 0; i < str.length; i++) {
+    str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
+  }
+  return str.join(' ');
+}
+
+console.log(titleCase("I'm a little tea pot"));
+
+/*
+
+2. With map method
+
+function titleCase(str) {
+  return str.toLowerCase().split(' ').map(function(word) {
+    return (word.charAt(0).toUpperCase() + word.slice(1));
+  }).join(' ');
+}
+
+*/
