8850131a0e311a9f459f5d9a59bf4835d9e5d3cc
Vishwas <gopinav5@gmail.com>
2018-05-27 13:34:49 +0530

    Find and remove array elements

---
+++ 14. Find and remove array elements/main.js
@@ -0,0 +1,15 @@
+function findAndRemove(arr) {
+  var targetArr = arguments[0];
+  var elementsArr = [];
+
+  // Create an array of all elements to be removed
+  for (var i = 1; i < arguments.length; i++)
+    elementsArr.push(arguments[i]);
+  
+  // return elements that do not match (indexOf returns -1)
+  return arr.filter(function(element) {
+    return params.indexOf(element) < 0;
+  });
+}
+
+console.log(findAndRemove([1, 2, 3, 1, 2, 3], 2, 3));
